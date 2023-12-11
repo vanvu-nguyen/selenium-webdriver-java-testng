ec13936b7fa12d2c8e0768d0b538b0d14452d46f
vanvu-nguyen <nv.vu29@gmail.com>
2023-12-11 21:21:41 +0700

    Login Practice

--- selenium/webdriver/Topic_10_Practice_Login.java
+++ selenium/webdriver/Topic_10_Practice_Login.java
@@ -1,5 +1,6 @@
 package webdriver;
 
+import net.bytebuddy.asm.Advice;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -9,6 +10,7 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
+import java.util.Random;
 
 public class Topic_10_Practice_Login {
     WebDriver driver;
@@ -72,6 +74,58 @@ public class Topic_10_Practice_Login {
         Assert.assertEquals(driver.findElement(By.xpath("//li[@class='error-msg']//span")).getText(),"Invalid login or password.");
     }
 
+    @Test
+    public void TC_05_Successful_Login() {
+        // Creating account
+        driver.get("http://live.techpanda.org/");
+        driver.findElement(By.xpath("//div[@class='footer']//a[@title='My Account']")).click();
+        sleepInSecond(2);
+        driver.findElement(By.xpath("//a[@title='Create an Account']")).click();
+        sleepInSecond(2);
+        String firstName = "vu", lastName = "nguyen", emailAddress = getEmailAddress(), passWord = "123456789";
+        driver.findElement(By.id("firstname")).sendKeys(firstName);
+        driver.findElement(By.id("lastname")).sendKeys(lastName);
+        driver.findElement(By.id("email_address")).sendKeys(emailAddress);
+        driver.findElement(By.id("password")).sendKeys(passWord);
+        driver.findElement(By.id("confirmation")).sendKeys(passWord);
+        driver.findElement(By.xpath("//button[@title='Register']")).click();
+        sleepInSecond(2);
+
+        // Check successful register notification
+        Assert.assertEquals(driver.findElement(By.xpath("//li[@class='success-msg']//span")).getText(), "Thank you for registering with Main Website Store.");
+        String fullName = firstName + " " + lastName;
+        Assert.assertEquals(driver.findElement(By.xpath("//div[@class='welcome-msg']//strong")).getText(), "Hello, " + fullName + "!");
+        String contactInfo = driver.findElement(By.xpath("(//a[text()='Change Password']/ancestor::p)[last()]")).getText();
+        Assert.assertTrue(contactInfo.contains(fullName));
+        Assert.assertTrue(contactInfo.contains(emailAddress));
+
+        // Logout
+        driver.findElement(By.xpath("//a[@class='skip-link skip-account']")).click();
+        sleepInSecond(2);
+        driver.findElement(By.xpath("//a[@title='Log Out']")).click();
+        sleepInSecond(10);
+
+        // Login
+        driver.findElement(By.xpath("//div[@class='footer']//a[@title='My Account']")).click();
+        driver.findElement(By.id("email")).sendKeys(emailAddress);
+        driver.findElement(By.id("pass")).sendKeys(passWord);
+        driver.findElement(By.id("send2")).click();
+        sleepInSecond(2);
+
+        // Check the profile info
+        Assert.assertEquals(driver.findElement(By.xpath("//div[@class='welcome-msg']//strong")).getText(), "Hello, " + fullName + "!");
+        contactInfo = driver.findElement(By.xpath("(//a[text()='Change Password']/ancestor::p)[last()]")).getText();
+        Assert.assertTrue(contactInfo.contains(fullName));
+        Assert.assertTrue(contactInfo.contains(emailAddress));
+
+        // Check account info
+        driver.findElement(By.xpath("//a[text()='Account Information']")).click();
+        sleepInSecond(2);
+        Assert.assertEquals(driver.findElement(By.id("firstname")).getAttribute("value"), firstName);
+        Assert.assertEquals(driver.findElement(By.id("lastname")).getAttribute("value"), lastName);
+        Assert.assertEquals(driver.findElement(By.id("email")).getAttribute("value"), emailAddress);
+    }
+
     @AfterClass
     public void afterClass() {
         driver.quit();
@@ -83,5 +137,10 @@ public class Topic_10_Practice_Login {
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public String getEmailAddress() {
+        Random randNum = new Random();
+        return "vunguyen" + randNum.nextInt(99999) + "@gmail.com";
     }
 }
